b0593b2ced46a73a686cad8a2549486a4a4f9296
Narahari Rao <narahari387@gmail.com>
2020-07-01 17:40:16 -0700

    styling changes, comments, and renaming

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,68 +14,60 @@
 
 package com.google.sps.servlets;
 
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-    DatastoreService datastore;
+  DatastoreService datastore;
+  Gson gson;
 
-    public DataServlet() {
-        datastore = DatastoreServiceFactory.getDatastoreService();
+  public DataServlet() {
+    datastore = DatastoreServiceFactory.getDatastoreService();
+    gson = new Gson();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    int maxComments = Integer.parseInt(request.getParameter("maxcomments"));
+    ArrayList<String> comments = new ArrayList<String>();
+
+    // Sort the query so we will be adding newest comments first
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    // Gets content of newest comments in according to maxComments specified by request
+    for (Entity commentEntity : results.asIterable()) {
+      if (comments.size() == maxComments) break;
+      comments.add((String) commentEntity.getProperty("content"));
     }
 
-    private String convertToJsonUsingGson(Object obj) {
-        Gson gson = new Gson();
-        String json = gson.toJson(obj);
-        System.out.println(json);
-        return json;
-    }
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+  }
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int maxComments = Integer.parseInt(request.getParameter("maxcomments"));
-        System.out.println(maxComments);
-        ArrayList<String> comments = new ArrayList<String>();
-        Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
-        System.out.println("comments query starting");
-        PreparedQuery results = datastore.prepare(query);
-        System.out.println("comments queried");
-        for (Entity commentEntity : results.asIterable()) {
-            comments.add((String) commentEntity.getProperty("content"));
-            if(comments.size() == maxComments)
-                break;
-        }
-        response.setContentType("application/json;");
-        response.getWriter().println(convertToJsonUsingGson(comments));
-    }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String content = request.getParameter("comment-input");
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String content = request.getParameter("comment-input");
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("content", content);
+    commentEntity.setProperty("timestamp", System.currentTimeMillis());
 
-        Entity commentEntity = new Entity("Comment");
-        commentEntity.setProperty("content", content);
-        commentEntity.setProperty("timestamp", System.currentTimeMillis());
-
-        datastore.put(commentEntity);
-    }
-
+    datastore.put(commentEntity);
+  }
 }
--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -14,41 +14,35 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
-
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/delete-data")
 public class DeleteDataServlet extends HttpServlet {
 
-    DatastoreService datastore;
+  DatastoreService datastore;
 
-    public DeleteDataServlet() {
-        datastore = DatastoreServiceFactory.getDatastoreService();
-    }
+  public DeleteDataServlet() {
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Query query = new Query("Comment");
-        PreparedQuery results = datastore.prepare(query);
-        for (Entity commentEntity : results.asIterable()) {
-            datastore.delete(commentEntity.getKey());
-        }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment");
+
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity commentEntity : results.asIterable()) {
+      datastore.delete(commentEntity.getKey());
     }
+  }
 }
